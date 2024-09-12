36a6e77c9fe153d83d6aa73b5bc01f5f61765971
ahadalichowdhury <smahadalichowdhury@gmail.com>
2024-09-12 17:59:07 +0600

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <div>
-        ahad ali chowdhury
+        bangladesh
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
